9d54d18ac89d44cde3f4589f0511872c55b555a5
idohyeon <ehgus5026@naver.com>
2026-02-21 20:37:03 +0900

    -Stream 커스텀 클래스 활용(Generic 타입 추가)
    ## 내용
    1. Stream 커스텀 클래스 타입을 Generic 타입으로 추가

---
+++ src/lambda/lambda5/mystream/MyStreamV3.java
@@ -0,0 +1,45 @@
+package lambda.lambda5.mystream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+// Generic 추가
+public class MyStreamV3<T> {
+
+    private List<T> internalList;
+
+    // 기본 생성자를 외부에서 직접 사용하지 못 하도록 접근제어자를 private로 수정
+    private MyStreamV3(List<T> internalList) {
+        this.internalList = internalList;
+    }
+
+    // static factory(정적 팩토리 메서드): 객체 생성을 담당하는 static 메서드
+    // 생성자 대신 of() 메서드를 이용하여 인스턴스를 생성하고 반환하는 역할을 함.
+    public static <T> MyStreamV3<T> of(List<T> internalList) {
+        return new MyStreamV3<>(internalList);
+    }
+
+    public MyStreamV3<T> filter(Predicate<T> predicate) {
+        List<T> filtered = new ArrayList<>();
+        for (T element : internalList) {
+            if (predicate.test(element)) {
+                filtered.add(element);
+            }
+        }
+        return MyStreamV3.of(filtered);
+    }
+
+    public <R> MyStreamV3<R> map(Function<T, R> mapper) {
+        List<R> mapped = new ArrayList<>();
+        for (T element : internalList) {
+            mapped.add(mapper.apply(element));
+        }
+        return MyStreamV3.of(mapped);
+    }
+
+    public List<T> toList() {
+        return internalList;
+    }
+}
